e23618a244bf69a306f845e44eb791b6f54bb2ac
Sunjun Ok <remedime0@gmail.com>
2023-02-13 16:09:28 +0900

    어드바이스 동작시험

---
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -0,0 +1,23 @@
+package com.springbook.biz.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
+
+public class AroundAdvice {
+	
+	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
+		
+		String method = pjp.getSignature().getName();
+		
+		StopWatch stopwatch = new StopWatch();
+		stopwatch.start();
+		
+		Object obj = pjp.proceed();
+		
+		stopwatch.stop();
+		System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopwatch.getTotalTimeMillis() + "(ms)초");
+		
+		return obj;		
+	}
+
+}
