f28ef4df22157c5822a4b3daece5f90b889ed560
Sunjun Ok <remedime0@gmail.com>
2023-02-15 14:31:18 +0900

    어노테이션 기반 AOP

--- src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,16 +1,22 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
 
 import com.springbook.biz.user.UserVO;
 
+@Service
+@Aspect
 public class AfterReturningAdvice {
 	
+	@AfterReturning(pointcut = "PointcutCommon.getPointcut()", returning = "returnObj")
 	public void afterLog(JoinPoint jp, Object returnObj) {
 		
 		String method = jp.getSignature().getName();
 		
-		if(returnObj instanceof UserVO) {
+		if(returnObj instanceof UserVO) {			
 			UserVO user = (UserVO) returnObj;
 			
 			if(user.getRole().equals("Admin")) {
@@ -18,7 +24,7 @@ public class AfterReturningAdvice {
 			}			
 		}
 		
-		System.out.println("[사후 처리] " + method + " () 메소드 리턴값 : " + returnObj.toString());
+		System.out.println("[사후 처리] " + method + "() 메소드 리턴값 : " + returnObj.toString());
 	}
 
 }
