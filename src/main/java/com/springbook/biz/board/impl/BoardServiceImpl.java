0fb699b81266dcb1a6a1f5c57b930392c68e2375
Sunjun Ok <remedime0@gmail.com>
2023-02-22 13:12:02 +0900

    프레젠테이션 레이어와 비즈니스 레이어 통합

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -11,7 +11,7 @@ import com.springbook.biz.board.BoardVO;
 public class BoardServiceImpl implements BoardService {
 	
 	@Autowired
-	private BoardDAOSpring boardDAO;
+	private BoardDAO boardDAO;
 	
 	public void insertBoard(BoardVO vo) {
 		boardDAO.insertBoard(vo);
